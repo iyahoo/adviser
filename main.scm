27ba3fb4893a4f9ff31b4ac5050384cc8de60723
phaendal <phaendal@gmail.com>
2017-05-22 21:37:16 +0900

    fix-iroiro

--- main.scm
+++ main.scm
@@ -1,3 +1,4 @@
+(use srfi-1)
 (use srfi-27)                           ; for 乱数
 (use util.list)
 
@@ -8,23 +9,28 @@
 
 (define (good-effect-message?)
   (print "もし提案した手法が効果があると感じた場合は \"g\" を入力してください")
-  (string=? (read) "g"))
+  (eq? (read) 'g))
+
+(define (read-file fname)
+  (with-input-from-file fname (lambda [] (read))))
 
 
 
-(define (main)
-  (let ((database (load "./database.scm")))
-    (print "続けますか？(\"exit\" or \"other\")")
-    (if (not (string=? (read) "exit"))
-        (let ((num (random-integer (length databes))) ; 貢献度に依存させたい
-              (message (car (nth num database)))
-              (contribution (car (cdr (nth num database)))))
-          (print message)
-          (if (good-effect-message?)
-              (let (new-database (update-nth-assoc database num (+ 1 contribution)))
-                (save-file new-database "./database.scm")
-                (main))
-              (main))))))
+(define (main args)
+  (let* ([database (read-file "./database.scm")]
+	 [dlen     (length database)])
+    (let loop []
+      (print "続けますか？(\"exit\" or \"other\")")
+      (if (not (eq? (read) 'exit))
+	  (let* ([num (random-integer dlen)] ; 貢献度に依存させたい
+		 [entry (list-ref database num)]
+		 [message (car entry)]
+		 [contribution (cdr entry)])
+	    (print message)
+	    (if (good-effect-message?)
+		(let ((new-database (update-nth-assoc database num (+ 1 contribution))))
+		  (save-file new-database "./database.scm")
+		  (loop))
+		(loop)))))))
 
 ;; Issues 実行不可 (update-nth-assoc) (save-file)  同じ質問くる
-
