e9fae9108c6bbd5393d729aaa7770c534de97b6d
iyahoo <s1200191@gmail.com>
2017-05-22 22:44:24 +0900

    :memo: :lipstick:

--- main.scm
+++ main.scm
@@ -8,29 +8,27 @@
       (nth (- n 1) (cdr lst))))
 
 (define (good-effect-message?)
-  (print "もし提案した手法が効果があると感じた場合は \"g\" を入力してください")
+  (print "もし提案した手法が効果があると感じた場合は g を入力してください")
   (eq? (read) 'g))
 
 (define (read-file fname)
   (with-input-from-file fname (lambda [] (read))))
 
-
-
 (define (main args)
   (let* ([database (read-file "./database.scm")]
-	 [dlen     (length database)])
+         [dlen     (length database)])
     (let loop []
-      (print "続けますか？(\"exit\" or \"other\")")
+      (print "続けますか？(exit or other)")
       (if (not (eq? (read) 'exit))
-	  (let* ([num (random-integer dlen)] ; 貢献度に依存させたい
-		 [entry (list-ref database num)]
-		 [message (car entry)]
-		 [contribution (cdr entry)])
-	    (print message)
-	    (if (good-effect-message?)
-		(let ((new-database (update-nth-assoc database num (+ 1 contribution))))
-		  (save-file new-database "./database.scm")
-		  (loop))
-		(loop)))))))
+          (let* ([num (random-integer dlen)] ; 貢献度に依存させたい
+                 [entry (list-ref database num)]
+                 [message (car entry)]
+                 [contribution (cdr entry)])
+            (print message)
+            (if (good-effect-message?)
+                (let ((new-database (update-nth-assoc database num (+ 1 contribution))))
+                  (save-file new-database "./database.scm")
+                  (loop))
+                (loop)))))))
 
 ;; Issues 実行不可 (update-nth-assoc) (save-file)  同じ質問くる
