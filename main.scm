925034b8d593b36748626b43e65e4f69ad83202c
iyahoo <s1200191@gmail.com>
2017-05-23 15:13:37 +0900

    :memo:

--- main.scm
+++ main.scm
@@ -3,6 +3,8 @@
 (use util.list)
 (use util.match)                        ; like destructuring-bind
 
+(define *database-file-path* "./database.scm")
+
 (define (good-effect-message?)
   (print "もし提案した手法が効果があると感じた場合は g を入力してください")
   (eq? (read) 'g))
@@ -14,12 +16,16 @@
   (with-output-to-file fname (lambda [] (write data))
                        :if-exists :supersede))
 
+(define (delete-duplicate-assoc-keys alist)
+  (delete-duplicates alist (lambda (a b) (equal? (car a) (car b)))))
+
 (define (a-process database keys-len)
   (print "調子はどうですか？(good, bad or exit. 他は bad として認識されます)")
   (let ([command (read)])
     (cond [(eq? command 'good)
            (a-process database keys-len)]
           [(eq? command 'exit)
+           (save-file *database-file-path* (delete-duplicate-assoc-keys database))
            (print "終了します")]
           [else
            (let* ([target-id (random-integer keys-len)] ; 貢献度に依存させたい
@@ -28,13 +34,12 @@
                (print message)
                (if (good-effect-message?)
                    (let ([new-database (alist-cons id (list message (+ 1 contribution)) database)])
-                     (save-file "./database.scm" new-database)
-                     (a-process database keys-len))
+                     (a-process new-database keys-len))
                    (a-process database keys-len))))])))
 
 (define main
   (lambda args
-    (let* ([database (read-file "./database.scm")]
+    (let* ([database (read-file *database-file-path*)]
            [keys (delete-duplicates (map (lambda [lst] (car lst)) database))]
            [keys-len (length keys)])
       (a-process database keys-len))))
