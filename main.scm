4c0a5f755168ea0064c25a9dbeeff35616c589b2
iyahoo <s1200191@gmail.com>
2017-05-23 15:01:31 +0900

    :memo: :tada:

--- main.scm
+++ main.scm
@@ -1,6 +1,7 @@
 (use srfi-1)
 (use srfi-27)                           ; for 乱数
 (use util.list)
+(use util.match)                        ; like destructuring-bind
 
 (define (good-effect-message?)
   (print "もし提案した手法が効果があると感じた場合は g を入力してください")
@@ -9,21 +10,31 @@
 (define (read-file fname)
   (with-input-from-file fname (lambda [] (read))))
 
-(define (main args)
-  (let* ([database (read-file "./database.scm")]
-         [dlen     (length database)])
-    (let loop []
-      (print "続けますか？(exit or other)")
-      (if (not (eq? (read) 'exit))
-          (let* ([num (random-integer dlen)] ; 貢献度に依存させたい
-                 [entry (list-ref database num)]
-                 [message (car entry)]
-                 [contribution (cdr entry)])
-            (print message)
-            (if (good-effect-message?)
-                (let ((new-database (update-nth-assoc database num (+ 1 contribution))))
-                  (save-file new-database "./database.scm")
-                  (loop))
-                (loop)))))))
+(define (save-file fname data)
+  (with-output-to-file fname (lambda [] (write data))
+                       :if-exists :supersede))
 
-;; Issues 実行不可 (update-nth-assoc) (save-file)  同じ質問くる
+(define (a-process database keys-len)
+  (print "調子はどうですか？(good, bad or exit. 他は bad として認識されます)")
+  (let ([command (read)])
+    (cond [(eq? command 'good)
+           (a-process database keys-len)]
+          [(eq? command 'exit)
+           (print "終了します")]
+          [else
+           (let* ([target-id    (random-integer keys-len)] ; 貢献度に依存させたい
+                  [entry        (assoc target-id database)])
+             (match-let1 (id message contribution) entry
+               (print message)
+               (if (good-effect-message?)
+                   (let ([new-database (alist-cons id (list message (+ 1 contribution)) database)])
+                     (save-file "./database.scm" new-database)
+                     (a-process database keys-len))
+                   (a-process database keys-len))))])))
+
+(define main
+  (lambda args
+    (let* ([database (read-file "./database.scm")]
+           [keys (delete-duplicates (map (lambda [lst] (car lst)) database))]
+           [keys-len (length keys)])
+      (a-process database keys-len))))
