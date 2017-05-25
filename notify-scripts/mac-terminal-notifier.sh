c422d53fa8b61022f4a4d557927140331147a29d
phaendal <phaendal@gmail.com>
2017-05-26 00:16:59 +0900

    notify

---
+++ notify-scripts/mac-terminal-notifier.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# terminal-notifier (https://github.com/julienXX/terminal-notifier) を利用してデスクトップに通知します。
+# $ brew install terminal-notifier でインストールできます。
+# https://github.com/julienXX/terminal-notifier/blob/master/README.markdown にある通り通知設定をすることを推奨します (Action が使えないとすぐに通知が消えてしまい見逃すため)。
+
+terminal-notifier -message $1 -closeLabel Close
